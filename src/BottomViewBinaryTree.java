bf089c86e89c76e4af92f1df6bb71b0a5c59c988
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-20 18:45:07 -0700

    created a node classs

--- src/BottomViewBinaryTree.java
+++ src/BottomViewBinaryTree.java
@@ -1,5 +1,18 @@
 public class BottomViewBinaryTree {
+
+
+
+    class Node {
+        int data;
+        Node node;
+        Node(Node node, int data){
+            this.data = data;
+            this.node = node;
+        }
+    }
+
+
     public static void main(String[] args) {
-        
+
     }
 }
