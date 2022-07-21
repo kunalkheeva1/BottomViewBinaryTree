b662f7db9e92d2719a38875e69c5db810a819139
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-20 18:55:03 -0700

    coded the method to return the elemets of the bottom view of the tree

--- src/BottomViewBinaryTree.java
+++ src/BottomViewBinaryTree.java
@@ -1,13 +1,18 @@
+import java.nio.channels.Pipe;
+import java.util.*;
+
 public class BottomViewBinaryTree {
 
 
 
     class Node {
         int data;
-        Node node;
-        Node(Node node, int data){
+        Node left;
+        Node right;
+        Node(Node left,Node right, int data){
             this.data = data;
-            this.node = node;
+            this.left = left;
+            this.right = right;
         }
     }
     class Pair{
@@ -17,6 +22,35 @@ public class BottomViewBinaryTree {
             this.node = node;
             this.val = val;
         }
+    }
+
+    public List<Integer> bottomViewBinaryTree(Node root){
+        ArrayList<Integer> ans = new ArrayList<>();
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        if(root == null) return ans;
+
+        Queue<Pair> q = new LinkedList<>();
+        Pair pair = new Pair(root, 0);
+        q.add(pair);
+
+        while(!q.isEmpty()){
+            Pair pp = q.remove();
+            Node current = pp.node;
+            int value = pp.val;
+            if(current.left != null){
+                Pair p1 = new Pair(current.left, value-1);
+                q.add(p1);
+            }
+            if(current.right != null){
+                Pair p2 = new Pair(current.right, value+1);
+                q.add(p2);
+            }
+            map.put(value, current.data);
+        }
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+            ans.add(entry.getValue());
+        }return ans;
+
     }
 
 
